c8309587b4a99a752f9f2bd7ffaba0c28c67a2f5
Arsene Musafiri INGENZI <209242850+ArseneIn@users.noreply.github.com>
2026-05-16 06:45:31 +0200

    updated cors

--- Smart_Curuza/backend/src/app.module.ts
+++ Smart_Curuza/backend/src/app.module.ts
@@ -34,7 +34,7 @@ const imports: any[] = [
       username: configService.get<string>('DB_USERNAME'),
       password: configService.get<string>('DB_PASSWORD'),
       database: configService.get<string>('DB_NAME'),
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
       synchronize: configService.get<string>('NODE_ENV') !== 'production',
       ssl:
         configService.get<string>('DB_SSL') === 'true'
